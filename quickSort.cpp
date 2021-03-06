b2f841f4dab680230393342b835423f43e49a7eb
WhatIsSurprise <75518712+WhatIsSurprise@users.noreply.github.com>
2021-03-06 09:54:14 +0800

    Quick sort code(C++)
    给定你一个长度为n的整数数列。
    
    请你使用快速排序对这个数列按照从小到大进行排序。
    
    并将排好序的数列按顺序输出。
    
    输入格式
    输入共两行，第一行包含整数 n。
    
    第二行包含 n 个整数（所有整数均在1~109范围内），表示整个数列。
    
    输出格式
    输出共一行，包含 n 个整数，表示排好序的数列。
    数据范围
    1≤n≤100000
    输入样例：
    5
    3 1 2 4 5
    输出样例：
    1 2 3 4 5
    *************************************from AcWing*****************************************

---
+++ quickSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+void quickSort(int p[], int l, int r)
+{
+    if(l>=r)
+    {
+        return;
+    }
+
+    int mid = p[l+r>>1];
+    int i=l-1,j=r+1;
+    while(i<j)
+    {
+        do{i++;} while(p[i]<mid);
+        do{j--;} while(p[j]>mid);
+        
+        if(i<j)
+        {
+            int t = p[i];
+            p[i] = p[j];
+            p[j] = t;
+        }
+        
+    }
+    quickSort(p,l,j);
+    quickSort(p,j+1,r);
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *p = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>p[i];
+    }
+
+    quickSort(p,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<p[i]<<" ";
+    }
+    return 0;
+}
